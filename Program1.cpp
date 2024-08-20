4289c5320d7aedb0e838449f1d517182d741e995
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-08-20 20:15:28 +0530

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,18 @@
+//C++ Hello World
+
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+    void helloWorld() {
+        cout << "Hello World";
+    }
+};
+
+int main() {
+    Solution sol;
+    sol.helloWorld();  // Call the helloWorld method
+    return 0;
+}
