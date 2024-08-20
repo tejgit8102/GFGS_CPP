43447e7fb21de8507a2165aff65ed85f563f7646
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-08-20 21:17:34 +0530

    Add files via upload

---
+++ Program9.cpp
@@ -0,0 +1,34 @@
+//Start Coding
+
+#include <iostream> // Include necessary header for input and output
+using namespace std; // Use the standard namespace
+
+// Class definition
+class Solution {
+  public:
+    void printHello() {
+        cout << "Hello World"; // Print the message
+    }
+};
+
+// Main function
+int main() {
+    Solution sol; // Create an object of Solution class
+    sol.printHello(); // Call the printHello method
+    
+    return 0; // Return 0 to indicate successful execution
+}
+
+/*
+We write only the below code in the editor of GFGS
+
+class Solution {
+  public:
+    void printHello() {
+        
+        cout<<"Hello World";
+    }
+};
+
+
+*/
