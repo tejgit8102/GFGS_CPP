ddd77ebbd471f911b7c827067e07a424a8b5f100
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-08-20 21:02:09 +0530

    Add files via upload

---
+++ Program2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Function to print each word on a new line
+void printNewLine() {
+    cout << "Geeks" << endl;  // Print "Geeks" and move to the next line
+    cout << "for" << endl;    // Print "for" and move to the next line
+    cout << "Geeks" << endl;  // Print "Geeks" and move to the next line
+}
+
+int main() {
+    printNewLine();  // Call the function to print the words
+
+    return 0;  // Return 0 to indicate successful execution
+}
+
+
+/*
+We write only the below code in the editor of GFGS
+
+void printNewLine() {
+    cout << "Geeks" << endl;  // Print "Geeks" and move to the next line
+    cout << "for" << endl;    // Print "for" and move to the next line
+    cout << "Geeks" << endl;  // Print "Geeks" and move to the next line
+}
+
+*/
